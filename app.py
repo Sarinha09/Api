d80b9ef84b7320f2f70485e248b1aa457514126b
Sara Dutra <62157062+Sarinha09@users.noreply.github.com>
2025-10-05 15:14:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -198,5 +198,7 @@ def random_tree_image():
 
 # O bloco abaixo é apenas para testes locais. O Render não o utilizará.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
 
